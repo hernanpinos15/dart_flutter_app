c8fbd2d082b410cf1376bccb74a0590d9dce779b
HERNAN PINOS <hernan.pinos@outlook.com>
2020-05-21 15:42:27 -0500

    Random de palabras en ingles

--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,15 +7,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'My App HP',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('My App HP'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          // child: Text('Empezamos'),
+          child: RandomWords(),
         ),
       ),
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context){
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}  
+
+class RandomWords extends StatefulWidget {
+  @override 
+  RandomWordsState createState() => new RandomWordsState();
 }
